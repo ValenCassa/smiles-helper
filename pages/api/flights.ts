807030d735bb5d1d6e76877c561992ac2dca4406
Valentin Cassarino <valencassa@MacBook-Pro-de-Valentin.local>
2022-09-09 18:15:14 -0300

    Fix headers

--- pages/api/flights.ts
+++ pages/api/flights.ts
@@ -7,15 +7,6 @@ export default async function handler(
 ) {
   const { method, headers } = req;
 
-  if (
-    (process.env.NODE_ENV === "development" &&
-      headers.host !== "localhost:3000") ||
-    (process.env.NODE_ENV === "production" &&
-      !headers.host?.includes("valencassa.dev"))
-  ) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
   if (method === "GET") {
     try {
       const data = await flightScrapping({ req, res });
